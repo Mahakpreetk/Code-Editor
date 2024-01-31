d7f42166bb28d422c2048e81ea73c63c3c79f7a4
Mahakpreet Kaur <kaurmahakpreet0520@gmail.com>
2024-01-31 03:17:13 -0500

    some-changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { AppBar, Toolbar, styled } from "@mui/material";
 
 const Container = styled(AppBar)`
-  background: #12486b;
+  background: darkblue;
   position: static;
   border-bottom: 1px solid #2f2f2f;
   height: 9vh;
@@ -9,12 +9,12 @@ const Container = styled(AppBar)`
 
 const Header = () => {
   const logo =
-    "https://images-eds-ssl.xboxlive.com/image?url=Q_rwcVSTCIytJ0KOzcjWTYtI_MIrVq4WfN7M.qN7gV3ayNiQeJK6Uxg366DH3bnRmVWMFBWWyXonVyp6x0RYE1elb_jkQQQH7FwsNBBqQO4iFrOIwXtaGkMjmrISfBfgMsCEGwIBPArmzCSVWYx1zA--&format=source";
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm16cvpw1DAPwmeRC_tz40EMCUk0xuspBDodXjm5LG9W7I-l2W7N2nhRvyMMUVXavRUko&usqp=CAU";
   return (
     <Container>
       <Toolbar>
         <img src={logo} alt="logo" style={{ width: 40 }} />
-        <h3 style={{ marginLeft: "8px" }}>Code Editor</h3>
+        <h3 style={{ marginLeft: "45%" }}>Code Editor</h3>
       </Toolbar>
     </Container>
   );
